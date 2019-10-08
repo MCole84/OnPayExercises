40268df2c61a3e004f3c4fe1283420185d20f3c9
Michael <M_Cole84@hotmail.com>
2019-10-08 12:34:41 -0400

    modified tabs

--- OnPayProject/Project.UI/client-app/src/App.vue
+++ OnPayProject/Project.UI/client-app/src/App.vue
@@ -1,9 +1,118 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+    <tabs
+      :tabs="tabs"
+      :currentTab="currentTab"
+      :wrapper-class="'default-tabs'"
+      :tabs-class="'default-tabs__item'"
+      :tab-active-class="'default-tabs__active-line'"
+      :line-class="'default-tabs__active-line'"
+      @onClick="handleClick"
+    />
+    <div class="content" id="content">
+      <div v-if="currentTab == 'DashBoard'">
+        <h1>Dashboard</h1>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
+<script>
+import Tabs from "vue-tabs-with-active-line";
+
+const TABS = [
+  {
+    title: "Login",
+    value: "Login"
+  },
+  {
+    title: "Dashboard",
+    value: "Dashboard"
+  }
+];
+export default {
+  components: {
+    Tabs
+  },
+  data: () => ({
+    tabs: TABS,
+    currentTab: "Login",
+    loggedIn: false
+  }),
+  methods: {
+    handleClick(newTab) {
+      this.currentTab = newTab;
+      var content = document.getElementById("content");
+      if (this.currentTab == "Login") {
+        if (this.loggedIn) {
+          content.innerHTML = "<h1>You are currently Logged in</h1>";
+        } else {
+          content.innerHTML = "<h1>Please log in</h1>";
+        }
+      } else {
+        if (this.loggedIn) {
+        } else {
+          content.innerHTML = "<h1>Please log in to view your dashboard";
+        }
+      }
+    },
+    handleLogin(user, password) {
+      if (user === "test" && password === "password123") {
+        this.password = true;
+      }
+      var content = document.getElementById("content");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.default-tabs {
+  position: relative;
+  margin: 0 auto;
+  &__item {
+    display: inline-block;
+    margin: 0 5px;
+    padding: 10px;
+    padding-bottom: 8px;
+    font-size: 16px;
+    letter-spacing: 0.8px;
+    color: gray;
+    text-decoration: none;
+    border: none;
+    background-color: transparent;
+    border-bottom: 2px solid transparent;
+    cursor: pointer;
+    transition: all 0.25s;
+    &_active {
+      color: black;
+    }
+    &:hover {
+      border-bottom: 2px solid gray;
+      color: black;
+    }
+    &:focus {
+      outline: none;
+      border-bottom: 2px solid gray;
+      color: black;
+    }
+    &:first-child {
+      margin-left: 0;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  &__active-line {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 2px;
+    background-color: black;
+    transition: transform 0.4s ease, width 0.4s ease;
+  }
+}
+.content {
+  margin-top: 30px;
+  font-size: 20px;
+}
+</style>
